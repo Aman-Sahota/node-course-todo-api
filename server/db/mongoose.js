21a6185fa88567575f9c9379607d1bb73a3fbd08
unknown <amansahota1996@gmail.com>
2018-07-25 16:42:20 +0530

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+process.env.MONGODB_URI='mongodb://obito1996:22101996aman@ds245661.mlab.com:45661/todoapp_obito';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports=mongoose;
