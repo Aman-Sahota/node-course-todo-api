83acdc1ec9c76f2f15b3a717e17fe84cd5a7e46c
unknown <amansahota1996@gmail.com>
2018-07-25 17:02:52 +0530

    reset since heroku cannot be used

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-process.env.MONGODB_URI='mongodb://obito1996:22101996aman@ds245661.mlab.com:45661/todoapp_obito';
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports=mongoose;
